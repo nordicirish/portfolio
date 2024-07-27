3133ee9b116425d07c417a1c75dd09aa08a1069f
Roger Graham <rogergraham147@gmail.com>
2024-07-27 19:02:15 +0300

    Add top padding to ensure sections load beneath the fixed nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className}bg-gray-50 text-gray-950 relative h-[5000px]`}
+        className={`${inter.className}bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 h-[5000px]`}
       >
         {/* top gradient background tailwindcustom attributes */}
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
