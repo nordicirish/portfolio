7fc092d9cc9f91b4404e5fcf411adbe277377cc7
Roger Graham <rogergraham147@gmail.com>
2024-07-29 20:50:47 +0300

    Add Experience component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/about";
+import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
@@ -10,10 +11,9 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
-
       <Projects />
-
       <Skills />
+      <Experience />
     </main>
   );
 }
