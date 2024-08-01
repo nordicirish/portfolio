7d0af66cf211a7c85a671058ae8d250d3079b372
Roger Graham <rogergraham147@gmail.com>
2024-08-01 22:55:06 +0300

    Remove dynamic loading of project from projects and instead import projects dynamically on page.tsx as the original approach was causing rendering problems

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import About from "@/components/about";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
-import Projects from "@/components/projects";
+// import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
+import dynamic from "next/dynamic";
 
 export default function Home() {
+  const Projects = dynamic(() => import("@/components/projects"), { ssr: false });
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
--- components/projects.tsx
+++ components/projects.tsx
@@ -3,10 +3,8 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
-import dynamic from "next/dynamic";
+import Project from "./project";
 export default function Projects() {
-  // lazy loads the project - ssr false to prevent server side rendering as it is a client side component
-  const Project = dynamic(() => import("./project"), { ssr: false });
   const { ref } = useSectionInView("Projects", 0.5);
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28 ">
