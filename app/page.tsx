3421373cd379f091b118b288848f4484f540647b
Roger Graham <rogergraham147@gmail.com>
2024-08-01 23:14:54 +0300

    Revert Skills to to a static import
    Test affect on Lighthouse score

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // import About from "@/components/about";
 // import Experience from "@/components/experience";
-// import Intro from "@/components/intro";
+import Intro from "@/components/intro";
 // import Projects from "@/components/projects";
 // import SectionDivider from "@/components/section-divider";
 // import Skills from "@/components/skills";
@@ -9,18 +9,18 @@ import Footer from "@/components/footer";
 import dynamic from "next/dynamic";
 
 export default function Home() {
-  const Projects = dynamic(() => import("@/components/projects"), { ssr: false });
-  const Intro = dynamic(() => import("@/components/intro"), { ssr: false });
-  const SectionDivider = dynamic(
-    () => import("@/components/section-divider"),
-    { ssr: false }
-  )
+  const Projects = dynamic(() => import("@/components/projects"), {
+    ssr: false,
+  });
+  // const Intro = dynamic(() => import("@/components/intro"), { ssr: false });
+  const SectionDivider = dynamic(() => import("@/components/section-divider"), {
+    ssr: false,
+  });
   const About = dynamic(() => import("@/components/about"), { ssr: false });
   const Skills = dynamic(() => import("@/components/skills"), { ssr: false });
-  const Experience = dynamic(
-    () => import("@/components/experience"),
-    { ssr: false }
-  );
+  const Experience = dynamic(() => import("@/components/experience"), {
+    ssr: false,
+  });
   const Contact = dynamic(() => import("@/components/contact"), { ssr: false });
 
   return (
