d4a78d981da5cfc43a7cdc5d382d643a67f355e4
Roger Graham <rogergraham147@gmail.com>
2024-08-01 23:05:34 +0300

    Add dynamic imports to all client side components on the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
-import About from "@/components/about";
-import Experience from "@/components/experience";
-import Intro from "@/components/intro";
+// import About from "@/components/about";
+// import Experience from "@/components/experience";
+// import Intro from "@/components/intro";
 // import Projects from "@/components/projects";
-import SectionDivider from "@/components/section-divider";
-import Skills from "@/components/skills";
-import Contact from "@/components/contact";
+// import SectionDivider from "@/components/section-divider";
+// import Skills from "@/components/skills";
+// import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import dynamic from "next/dynamic";
 
 export default function Home() {
   const Projects = dynamic(() => import("@/components/projects"), { ssr: false });
+  const Intro = dynamic(() => import("@/components/intro"), { ssr: false });
+  const SectionDivider = dynamic(
+    () => import("@/components/section-divider"),
+    { ssr: false }
+  )
+  const About = dynamic(() => import("@/components/about"), { ssr: false });
+  const Skills = dynamic(() => import("@/components/skills"), { ssr: false });
+  const Experience = dynamic(
+    () => import("@/components/experience"),
+    { ssr: false }
+  );
+  const Contact = dynamic(() => import("@/components/contact"), { ssr: false });
+
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
