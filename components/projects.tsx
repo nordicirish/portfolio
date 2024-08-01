3fe9806371065575a969350a0e862bcadeb2d789
Roger Graham <rogergraham147@gmail.com>
2024-08-01 22:45:03 +0300

    Add dynamic import for project to make it lazy load and improve page performance

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
-import Project from "./project";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
-
+import dynamic from "next/dynamic";
 export default function Projects() {
+  // lazy loads the project - ssr false to prevent server side rendering as it is a client side component
+  const Project = dynamic(() => import("./project"), { ssr: false });
   const { ref } = useSectionInView("Projects", 0.5);
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28 ">
