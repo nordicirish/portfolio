7e7653bc769404fdda80a6b2259def2f540d7c8d
Roger Graham <rogergraham147@gmail.com>
2024-08-04 22:50:08 +0300

    set default theme to "light"

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -19,37 +19,37 @@ const ThemeContext = createContext<ThemeContextType | null>(null);
 export default function ThemeContextProvider({
   children,
 }: ThemeContextProviderProps) {
-  const [theme, setTheme] = useState<Theme>("dark");
+  const [theme, setTheme] = useState<Theme>("light");
 
- const toggleTheme = () => {
-   if (theme === "light") {
-     setTheme("dark");
-     //save theme in local storage to persist theme
-     window.localStorage.setItem("theme", "dark");
-     document.documentElement.classList.add("dark");
-     //add dark class to html element so tailwind can apply dark mode
-   } else {
-     setTheme("light");
-     window.localStorage.setItem("theme", "light");
-     document.documentElement.classList.remove("dark");
-     //remove dark class from html element
-   }
- };
- // check if theme is in local storage when page loads and set it
- useEffect(() => {
-   const localTheme = window.localStorage.getItem("theme") as Theme | null;
-   // as Theme specifies the type and values of localTheme
-   if (localTheme) {
-     setTheme(localTheme);
-     if (localTheme === "dark") {
-       document.documentElement.classList.add("dark");
-     }
-   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-     setTheme("dark");
-     document.documentElement.classList.add("dark");
-   }
-   //if no value in local storage set theme using the machine preference for dark or light mode
- }, []);
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      //save theme in local storage to persist theme
+      window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+      //add dark class to html element so tailwind can apply dark mode
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+      //remove dark class from html element
+    }
+  };
+  // check if theme is in local storage when page loads and set it
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    // as Theme specifies the type and values of localTheme
+    if (localTheme) {
+      setTheme(localTheme);
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    }
+    //if no value in local storage set theme using the machine preference for dark or light mode
+  }, []);
 
   return (
     <ThemeContext.Provider
