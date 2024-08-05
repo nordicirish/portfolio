8803a9297d68b982bc17ce8b13c81deec0af8f56
Roger Graham <rogergraham147@gmail.com>
2024-08-05 23:50:34 +0300

    Add max-w className

--- components/projects.tsx
+++ components/projects.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
     <SectionWithRef
       id="Projects"
       threshold={0.75}
-      className="scroll-mt-28 mb-28"
+      className="scroll-mt-28 mb-28 max-w-[53rem]"
     >
       <SectionHeading>My Projects</SectionHeading>
       <div>
