b22d579e80bfde3d868f78866e5676dad3a9aee7
Roger Graham <rogergraham147@gmail.com>
2024-08-05 23:48:05 +0300

    Restyle the h2 to add border bottom, gradient and tracking + leading

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
-    <h2 className="text-3xl font-medium capitalize mb-8 text-center text-blue-900 dark:text-gray-50 dark:text-opacity-90">
+    <h2 className="max-w-[42rem] w-full text-3xl font-medium capitalize mb-8 mx-auto text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 dark:from-gray-100 dark:to-gray-400 tracking-wide leading-tight px-4 border-b-2 border-blue-700 dark:border-gray-400">
       {children}
     </h2>
   );
