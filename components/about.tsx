135662a6ffb28f8676c62a7834ed11bb7e20c9e9
Roger Graham <rogergraham147@gmail.com>
2024-08-05 20:40:31 +0300

    Increase threshold value passed to SectionWithRef

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      threshold={0.6}
+      threshold={0.75}
       id="About"
     >
       <SectionHeading>About Me</SectionHeading>
