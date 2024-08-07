50650fd8521602d75b60a436027a973ebc59743d
Roger Graham <rogergraham147@gmail.com>
2024-08-07 21:16:14 +0300

    Dynamically import skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Footer from "@/components/footer";
 import dynamic from "next/dynamic";
 import Intro from "@/components/intro";
-import Skills from "@/components/skills";
+// import Skills from "@/components/skills";
 export default function Home() {
   const Projects = dynamic(() => import("@/components/projects"));
   const SectionDivider = dynamic(() => import("@/components/section-divider"));
   const About = dynamic(() => import("@/components/about"));
   const Experience = dynamic(() => import("@/components/experience"));
   const Contact = dynamic(() => import("@/components/contact"));
+  const Skills = dynamic(() => import("@/components/skills"));
 
   return (
     <main className="flex flex-col items-center px-4">
