dc8834e89b7c915fc9114f9a5423f5c862b58267
Roger Graham <rogergraham147@gmail.com>
2024-08-07 21:17:28 +0300

    Change animation settings

--- components/skills-components/skills-list.tsx
+++ components/skills-components/skills-list.tsx
@@ -2,37 +2,27 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Skill } from "@/lib/types";
-import { iconMap } from '@/lib/utils';
+import { iconMap } from "@/lib/utils";
 
 // client side component to render skills list
 interface SkillsListProps {
   skills: Skill[];
 }
 
-
-const fadeInAnimationVariants = {
+const fadeInSpinAnimationVariants = {
   initial: {
     opacity: 0,
-    y: 100,
+    y: 20,
+    rotateY: 0,
   },
   animate: (index: number) => ({
     opacity: 1,
     y: 0,
-    transition: {
-      delay: 0.15 * index, // Adjust delay for staggering effect
-      duration: 0.5, // Duration for fade-in effect
-    },
-  }),
-};
-
-const spinAnimationVariants = {
-  initial: { rotateY: 0 },
-  animate: (index: number) => ({
     rotateY: 360,
     transition: {
-      delay: 0.15 * index, // Delay based on the index to stagger the spin
-      duration: 1, // Duration of the spin animation in seconds
-      ease: "easeInOut", // Easing function
+      delay: index * 0.5, // Adjust delay for each item to stagger the animations
+      duration: 1.5, // Total duration for both animations
+      ease: "easeInOut",
     },
   }),
 };
@@ -49,26 +39,17 @@ const SkillsList: React.FC<SkillsListProps> = ({ skills }) => {
           <motion.li
             className="bg-amber-300 borderBlack shadow-light-mode dark:shadow-dark-mode rounded-full w-32 h-16 px-2 py-2 dark:bg-indigo-950 flex items-center justify-evenly flex-col sm:w-36 sm:h-18 sm:px-3 sm:py-3 md:w-44 md:h-20 md:px-4 md:py-4"
             key={index}
-            variants={fadeInAnimationVariants}
+            variants={fadeInSpinAnimationVariants}
             initial="initial"
             whileInView="animate"
-            viewport={{ once: true, amount: 0.1 }}
             custom={index}
+            viewport={{ once: true, amount: 0.1 }}
           >
-            <motion.div
-              className="bg-transparent grow"
-              variants={spinAnimationVariants}
-              initial="initial"
-              whileInView="animate"
-              viewport={{ once: true, amount: 0.1 }}
-              custom={index}
-            >
-              <IconComponent
-                size={30}
-                aria-label={`Icon depicting ${skill.skill} skill`}
-                role="img"
-              />
-            </motion.div>
+            <IconComponent
+              size={30}
+              aria-label={`Icon depicting ${skill.skill} skill`}
+              role="img"
+            />
             <span className="text-xs sm:text-sm md:text-base">
               {skill.skill}
             </span>
