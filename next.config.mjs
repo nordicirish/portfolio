e9d165678e2fdfabd2017838af22e5ca111c8bd4
Roger Graham <rogergraham147@gmail.com>
2024-08-12 17:02:28 +0300

    Add cache control vercel insights

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com",
-                          },
-        ]
-       
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/_vercel/insights/script.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600, stale-while-revalidate=15778476",
+            // Adjust max-age as needed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
