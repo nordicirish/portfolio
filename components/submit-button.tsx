710b9fe8b168e16329dde61917381f6b06c2288c
Roger Graham <rogergraham147@gmail.com>
2024-08-15 16:38:32 +0300

    Change shape and animation

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -9,8 +9,8 @@ export default function SubmitButton() {
       title="Submit the message"
       aria-label="Submit the message"
       type="submit"
-      className="group flex items-center justify-center h-[3rem] w-[8rem] bg-blue-800 outline-none text-white rounded-full transition-all focus:scale-110 hover:scale-110 hover:bg-blue-900 active:scale-105 disabled:scale-100 disabled:bg-opacity-65
-    dark:bg-fuchsia-900 dark:hover:bg-fuchsia-950 dark:active:bg-fuchsia-900 dark:disabled:bg-fuchsia-800 dark:disabled:bg-opacity-50 shadow-light-mode dark:shadow-dark-mode"
+      className="group flex items-center justify-center h-[3rem] w-[8rem] bg-blue-800 outline-none text-white rounded-lg transition-all duration-300 ease-out focus:scale-110 hover:scale-110 hover:bg-blue-900 active:scale-110 disabled:scale-100 disabled:bg-opacity-65
+    dark:bg-fuchsia-900 dark:hover:bg-fuchsia-950 dark:active:bg-fuchsia-900 dark:disabled:bg-fuchsia-800 dark:disabled:bg-opacity-50 shadow-lg dark:shadow-md"
       disabled={pending}
     >
       {/* disabled if pending is true */}
@@ -19,7 +19,7 @@ export default function SubmitButton() {
       ) : (
         <>
           Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+          <FaPaperPlane className="text-xs ml-1 opacity-70 transition group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
         </>
       )}
     </button>
