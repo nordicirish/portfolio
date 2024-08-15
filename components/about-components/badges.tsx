824bd981d685117e5188c9ba13892b2cf0cbfd0a
Roger Graham <rogergraham147@gmail.com>
2024-08-15 22:21:30 +0300

    Cahnge justify settings

--- components/about-components/badges.tsx
+++ components/about-components/badges.tsx
@@ -4,11 +4,11 @@ import { BadgesProps } from "@/lib/types";
 
 const Badges: React.FC<BadgesProps> = ({ badges }) => {
   return (
-    <div className="flex flex-wrap justify-evenly md:justify-center  gap-4 md:gap-8">
+    <div className="flex flex-wrap justify-center gap-4 md:gap-8">
       {badges.map((badge, index) => (
         <div
           key={index}
-          className="p-2 m-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer bg-gray-200 hover:bg-gray-300 border dark:border dark:border-white/10 dark:bg-gray-700 dark:hover:bg-gray-600 shadow-light-mode dark:shadow-dark-mode"
+          className="p-2 m-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-110 transition cursor-pointer bg-gray-200 hover:bg-gray-300 border dark:border dark:border-white/10 dark:bg-gray-700 dark:hover:bg-gray-600 shadow-light-mode dark:shadow-dark-mode"
         >
           <a href={badge.href} target="_blank" rel="noopener noreferrer">
             <Image
