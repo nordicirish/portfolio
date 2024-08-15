3bd12e7f5e426818dfc4e5ff07dd795baead1795
Roger Graham <rogergraham147@gmail.com>
2024-08-15 21:10:06 +0300

    Refactor animations and use UseIsMobile hook

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,9 +9,42 @@ import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useTheme } from "@/context/theme-context";
 import SectionWithRef from "./section-with-ref";
+import { useIsMobile } from "@/lib/hooks";
+const fadeUpAnimationDesktop = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  whileInView: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.35,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const fadeUpAnimationMobile = {
+  initial: { opacity: 0, y: 0 },
+  whileInView: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.35,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
 
 export default function Experience() {
+  const isMobile = useIsMobile();
   const { theme } = useTheme();
+  const fadeUpAnimation = isMobile
+    ? fadeUpAnimationMobile
+    : fadeUpAnimationDesktop;
   return (
     // id needs to match SectionName type and value in lib\types.ts
     // threshold ensures menu item only changes when section is 50% visible
@@ -20,17 +53,9 @@ export default function Experience() {
       id="Experience"
       threshold={0.5}
       className="scroll-mt-28 mb-28 sm:mb-32 w-full max-w-[50rem]"
-      initial={{ opacity: 0, y: 100 }}
+      initial={fadeUpAnimation.initial}
       viewport={{ once: true }}
-      whileInView={{
-        opacity: 1,
-        y: 0,
-        transition: {
-          delay: 0.35,
-          duration: 1,
-          ease: "easeInOut",
-        },
-      }}
+      whileInView={fadeUpAnimation.whileInView}
     >
       <SectionHeading>My Experience</SectionHeading>
       {/* lineColor="" to avoid browser warning */}
--- components/skills-components/skills-list.tsx
+++ components/skills-components/skills-list.tsx
@@ -3,48 +3,46 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Skill } from "@/lib/types";
 import { iconMap } from "@/lib/utils";
-
-import { useState, useEffect } from "react";
-
-const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    // Run on initial render
-    handleResize();
-
-    // Listen for resize changes
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup event listener on component unmount
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return isMobile;
-};
+import { useIsMobile } from "@/lib/hooks";
 
 // client side component to render skills list
 interface SkillsListProps {
   skills: Skill[];
 }
 
-const fadeInSpinAnimationVariants = {
+const animationVariantsDesktop = {
   initial: {
     opacity: 0,
     y: 20,
     rotateY: 0,
   },
-  animate: (index: number) => ({
+  whileInView: (index: number) => ({
     opacity: 0.25,
     y: 0,
     rotateY: 360,
     transition: {
       delay: index * 0.3, // Adjust delay for each item to stagger the animations
-      duration: 1.0, // Total duration for both animations
+      duration: 0.9, // Total duration for both animations
+      ease: "easeInOut",
+    },
+    transitionEnd: {
+      opacity: 1, // Return opacity to 1 after animation is complete
+    },
+  }),
+};
+const animationVariantsMobile = {
+  initial: {
+    opacity: 0,
+    y: 0,
+    rotateY: 0,
+  },
+  whileInView: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    rotateY: 0,
+    transition: {
+      delay: index * 0.3, // No delay on mobile
+      duration: 0.5, //
       ease: "easeInOut",
     },
     transitionEnd: {
@@ -55,40 +53,41 @@ const fadeInSpinAnimationVariants = {
 
 const SkillsList: React.FC<SkillsListProps> = ({ skills }) => {
   const isMobile = useIsMobile();
+  const animationVariants = isMobile
+    ? animationVariantsMobile
+    : animationVariantsDesktop;
   return (
-    <ul className="flex flex-wrap items-center max-w-[48rem] justify-center gap-2 text-lg dark:text-white/85 text-gray-900">
-      {skills.map((skill, index) => {
-        const IconComponent = iconMap[skill.icon]; // Get the icon component
+    <div className="flex items-center justify-center">
+      <ul className="flex col flex-wrap items-center max-w-[48rem] justify-center gap-2 text-lg dark:text-white/85 text-gray-900">
+        {skills.map((skill, index) => {
+          const IconComponent = iconMap[skill.icon]; // Get the icon component
 
-        if (!IconComponent) return null; // Skip rendering if no icon component
+          if (!IconComponent) return null; // Skip rendering if no icon component
 
-        return (
-          <motion.li
-            className="bg-gradient-to-r from-cyan-500 to-blue-500  shadow-light-mode dark:shadow-dark-mode rounded-full w-32 h-16 px-2 py-2 dark:bg-gradient-to-r dark:from-indigo-950 
-            dark:to-purple-950 flex items-center justify-evenly flex-col sm:w-36 sm:h-18 sm:px-3 sm:py-3 md:w-40 md:h-20 md:px-4 md:py-4"
-            key={index}
-            style={isMobile ? { opacity: 1, transform: "none" } : {}} // Ensure visibility on mobile
-            variants={!isMobile ? fadeInSpinAnimationVariants : undefined}
-            initial={!isMobile ? "initial" : false} // No initial state on mobile
-            // Explicitly set visible state on mobile
-            whileInView={
-              isMobile ? { opacity: 1, y: 0, rotateY: 0 } : "animate"
-            } // Ensure visibility on mobile
-            // Only apply `whileInView` for non-mobile devices
-            custom={index}
-            viewport={{ once: true, amount: 0.1 }}
-          >
-            <IconComponent
-              style={{ width: "100%", height: "100%", opacity: 1 }}
-              size={38}
-              aria-label={`Icon depicting ${skill.skill} skill`}
-              role="img"
-            />
-            <span className="text-sm">{skill.skill}</span>
-          </motion.li>
-        );
-      })}
-    </ul>
+          return (
+            <motion.li
+              className="bg-gradient-to-r from-cyan-300 to-blue-400 shadow-light-mode dark:shadow-dark-mode rounded-full w-32 h-16 px-2 py-2 dark:bg-gradient-to-r dark:from-cyan-800
+            dark:to-blue-950 flex items-center justify-evenly flex-col sm:w-36 sm:h-18 sm:px-3 sm:py-3 md:w-40 md:h-20 md:px-4 md:py-4"
+              key={index}
+              initial={animationVariants.initial}
+              whileInView={animationVariants.whileInView(index)}
+              // Ensure visibility on mobile
+              // Only apply `whileInView` for non-mobile devices
+              custom={index}
+              viewport={{ once: true, amount: 0.1 }}
+            >
+              <IconComponent
+                style={{ width: "100%", height: "100%", opacity: 1 }}
+                size={38}
+                aria-label={`Icon depicting ${skill.skill} skill`}
+                role="img"
+              />
+              <span className="text-sm">{skill.skill}</span>
+            </motion.li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
