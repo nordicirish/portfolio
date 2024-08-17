35041e1652a85c85bb7c9817f8c1e65c4fd9612d
Roger Graham <rogergraham147@gmail.com>
2024-08-17 22:59:39 +0300

    Change to show on sm screens

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function SectionDivider() {
   return (
     <motion.div
-      className="bg-gradient-to-r from-blue-500/10 to-blue-700/10 dark:from-gray-100/10 dark:to-gray-400/10 my-24 h-16 w-1 rounded-full hidden md:block"
+      className="bg-gradient-to-r from-blue-500/10 to-blue-700/10 dark:from-gray-100/10 dark:to-gray-400/10 my-24 h-16 w-1 rounded-full hidden sm:block"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
