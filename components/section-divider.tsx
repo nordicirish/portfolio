a1e047e4050173d4686831a4f7961bd664dd6ea7
Roger Graham <rogergraham147@gmail.com>
2024-08-17 23:15:55 +0300

    Use useIsMobile to check if mobile sceen and avoid rendering motion div

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -1,14 +1,20 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import { useIsMobile } from "@/lib/hooks";
 
 export default function SectionDivider() {
-  return (
-    <motion.div
-      className="bg-gradient-to-r from-blue-500/10 to-blue-700/10 dark:from-gray-100/10 dark:to-gray-400/10 my-24 h-16 w-1 rounded-full hidden sm:block"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.125 }}
-    ></motion.div>
-  );
+  const isMobile = useIsMobile();
+  if (!isMobile) {
+    return (
+      <motion.div
+        className="bg-gradient-to-r from-blue-500/10 to-blue-700/10 dark:from-gray-100/10 dark:to-gray-400/10 my-24 h-16 w-1 rounded-full hidden sm:block"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.125 }}
+      ></motion.div>
+    );
+  }
+
+  // If isMobile is true, the function will return undefined
 }
