4db1edd7429a881b7be054fda43736c7511253a7
Roger Graham <rogergraham147@gmail.com>
2024-08-17 23:26:12 +0300

    Change SectionDivider to dynamic import to investigate Lighthouse score change

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Footer from "@/components/footer";
 import dynamic from "next/dynamic";
 import Intro from "@/components/intro-components/intro";
-import SectionDivider from "@/components/section-divider";
+// import SectionDivider from "@/components/section-divider";
 
 // import Skills from "@/components/skills";
 export default function Home() {
+  const SectionDivider = dynamic(() => import("@/components/section-divider"));
   const Projects = dynamic(() => import("@/components/projects"));
   const About = dynamic(() => import("@/components/about-components/about"));
   const Experience = dynamic(() => import("@/components/experience"));
