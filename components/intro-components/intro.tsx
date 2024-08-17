3d54f29c9f5828883330b6907ef166985d9beae3
Roger Graham <rogergraham147@gmail.com>
2024-08-17 21:13:40 +0300

    Dynaically import framer.div

--- components/intro-components/intro.tsx
+++ components/intro-components/intro.tsx
@@ -4,8 +4,13 @@ import SectionWithRef from "../section-with-ref";
 import ProfileImage from "./profile-image";
 import IntroText from "./intro-text";
 import IntroLinks from "./intro-links";
-import { motion } from "framer-motion";
-
+// import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+// Dynamically import only framer div component
+const MotionDiv = dynamic(
+  () => import("framer-motion").then((mod) => mod.motion.div),
+  { ssr: false }
+);
 export default function Intro() {
   // id needs to match SectionName type and value in lib\types.ts
   // threshold ensures menu item only changes when section is 50% visible
@@ -16,27 +21,27 @@ export default function Intro() {
       threshold={0.5}
       className="mb-28 w-full max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <motion.div
+      <MotionDiv
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ type: "tween", duration: 0.2 }}
       >
         <ProfileImage />
-      </motion.div>
-      <motion.div
+      </MotionDiv>
+      <MotionDiv
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3 }}
       >
         <IntroText />
-      </motion.div>
-      <motion.div
+      </MotionDiv>
+      <MotionDiv
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 }}
       >
         <IntroLinks />
-      </motion.div>
+      </MotionDiv>
     </SectionWithRef>
   );
 }
