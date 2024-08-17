22cd04748e374ba20088eb4977ce5d730717e9e1
Roger Graham <rogergraham147@gmail.com>
2024-08-17 22:38:42 +0300

    Change threshold to accept optional mobileThreshold and desktopThrshold values and conditionally apply them based on useIsMobile return

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,8 +3,15 @@ import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { SectionName } from "./types";
 
-// add default threshold value of 0.75 if no threshold is provided
-export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+// add default threshold value of 0.75 for desktop and 0.4 for mobile if no threshold is provided
+export function useSectionInView(
+  sectionName: SectionName,
+  mobileThreshold: number = 0.4,
+  desktopThreshold: number = 0.75
+) {
+  const isMobile = useIsMobile();
+
+  const threshold = isMobile ? mobileThreshold : desktopThreshold;
   // useInView hook to make the active menu item change when scrolling
   const { ref, inView } = useInView(
     // threshold ensures menu item only changes when section is 75% visible
