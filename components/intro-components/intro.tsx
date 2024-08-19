c298d396b19884f8ef81a93cff1aeddf06276251
Roger Graham <rogergraham147@gmail.com>
2024-08-19 15:34:05 +0300

    remove dynamic motion import as is blocking content from rendering above the fold

--- components/intro-components/intro.tsx
+++ components/intro-components/intro.tsx
@@ -4,13 +4,8 @@ import SectionWithRef from "../section-with-ref";
 import ProfileImage from "./profile-image";
 import IntroText from "./intro-text";
 import IntroLinks from "./intro-links";
-// import { motion } from "framer-motion";
-import dynamic from "next/dynamic";
-// Dynamically import only framer div component
-const MotionDiv = dynamic(
-  () => import("framer-motion").then((mod) => mod.motion.div),
-  { ssr: false }
-);
+import { motion } from "framer-motion";
+
 export default function Intro() {
   // id needs to match SectionName type and value in lib\types.ts
   // threshold ensures menu item only changes when section is 50% visible
@@ -23,27 +18,27 @@ export default function Intro() {
       desktopThreshold={0.5}
       className="mb-28 w-full max-w-[50rem] text-center md:mb-0 scroll-mt-[100rem]"
     >
-      <MotionDiv
+      <motion.div
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ type: "tween", duration: 0.2 }}
       >
         <ProfileImage />
-      </MotionDiv>
-      <MotionDiv
+      </motion.div>
+      <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3 }}
       >
         <IntroText />
-      </MotionDiv>
-      <MotionDiv
+      </motion.div>
+      <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 }}
       >
         <IntroLinks />
-      </MotionDiv>
+      </motion.div>
     </SectionWithRef>
   );
 }
