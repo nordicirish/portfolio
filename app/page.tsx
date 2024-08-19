4676ec387d742413c02abe8795b9ab63bf516a6f
Roger Graham <rogergraham147@gmail.com>
2024-08-19 16:30:06 +0300

    Remove ssr false as appears to slow doen desktop lighthouse score

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import dynamic from "next/dynamic";
 import Intro from "@/components/intro-components/intro";
 import About from "@/components/about-components/about";
 
-
 export default function Home() {
-  const SectionDivider = dynamic(() => import("@/components/section-divider"), {
-    ssr: false,
-  });
+  const SectionDivider = dynamic(() => import("@/components/section-divider"));
   const Projects = dynamic(() => import("@/components/projects"));
   const Experience = dynamic(() => import("@/components/experience"));
   const Contact = dynamic(() => import("@/components/contact"));
