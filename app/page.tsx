f1660117e3577e66373d2495ade8e292bf1c88dc
Roger Graham <rogergraham147@gmail.com>
2024-08-19 15:57:56 +0300

    Change About to static import as it iis above the fold on desktop and contain SEO relevant info

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Footer from "@/components/footer";
 import dynamic from "next/dynamic";
 import Intro from "@/components/intro-components/intro";
-// import SectionDivider from "@/components/section-divider";
+import About from "@/components/about-components/about";
 
-// import Skills from "@/components/skills";
 export default function Home() {
   const SectionDivider = dynamic(() => import("@/components/section-divider"));
   const Projects = dynamic(() => import("@/components/projects"));
-  const About = dynamic(() => import("@/components/about-components/about"));
   const Experience = dynamic(() => import("@/components/experience"));
   const Contact = dynamic(() => import("@/components/contact"));
   const Skills = dynamic(() => import("@/components/skills-components/skills"));
