b92e99dd8bdc1ad41e474944580ebfff0aa0791b
Roger Graham <rogergraham147@gmail.com>
2024-08-19 20:17:05 +0300

    Resize image and apply border transparency

--- components/intro-components/profile-image.tsx
+++ components/intro-components/profile-image.tsx
@@ -3,18 +3,16 @@ import Image from "next/image";
 
 export default function ProfileImage() {
   return (
-    <div className="flex items-center justify-center relative">
-      <Image
-        src="/profile.png"
-        alt="Roger Graham portrait photo"
-        width={192}
-        height={192}
-        quality="95"
-        priority={true}
-        blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk2PHfGAAEYwHsXvURPgAAAABJRU5ErkJggg=="
-        placeholder="blur"
-        className="h-32 w-32 rounded-full border-[0.25rem] border-gradient-to-r from-blue-500 to-blue-700 object-cover shadow-lg dark:border-gradient-to-r dark:from-gray-700 dark:to-gray-500"
-      />
-    </div>
+    <Image
+      src="/profile.png"
+      alt="Roger Graham portrait photo"
+      width={176}
+      height={176}
+      quality="95"
+      priority={true}
+      blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk2PHfGAAEYwHsXvURPgAAAABJRU5ErkJggg=="
+      placeholder="blur"
+      className="h-44 w-44 rounded-full object-cover border-[0.35rem] border-white/40 shadow-xl "
+    />
   );
 }
