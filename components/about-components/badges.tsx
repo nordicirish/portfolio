1b4e5992335e8fb4a0a4adc761d1dfe0c3925fae
Roger Graham <rogergraham147@gmail.com>
2024-08-20 15:28:19 +0300

    Reduce gap below sm breakpoint

--- components/about-components/badges.tsx
+++ components/about-components/badges.tsx
@@ -3,7 +3,7 @@ import Badge from "@/components/about-components/badge";
 import { BadgesProps } from "@/lib/types";
 const Badges: React.FC<{ badges: BadgesProps[] }> = ({ badges }) => {
   return (
-    <div className="flex flex-wrap justify-center gap-4 md:gap-8">
+    <div className="flex flex-wrap justify-center gap-3 md:gap-8">
       {badges.map((badge, index) => (
         <Badge key={index} {...badge} />
       ))}
