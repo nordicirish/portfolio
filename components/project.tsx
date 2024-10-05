1d4a4bc3481b3566bd391773cadc73b0666a625d
Roger Graham <rogergraham147@gmail.com>
2024-10-05 18:39:49 +0300

    Change absolute positioning to flex

--- components/project.tsx
+++ components/project.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-//typeof obtains the types of individual elements projectsData objects array
 import Image from "next/image";
 import { useScroll, motion, useTransform } from "framer-motion";
 import React, { useRef } from "react";
 import { projectsData } from "@/lib/data";
+
 type ProjectProps = (typeof projectsData)[number];
 
 export default function Project({
@@ -16,19 +16,15 @@ export default function Project({
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
-    // target is where the animation should start
     target: ref,
-    // offset is how far from the target the animation should start and end. Ends when bottom of the viewport has gone 33% beyond the end of the target
     offset: ["0 1", "1.33 1"],
   });
-  // scale starts at 0.8 and ends at 1 so animation is not as exaggerated
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  // opacity starts at 0.6 and ends at 1 so animation is not as exaggerated
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
-    //need to use enclosing div so animation is smooth
     <motion.div
-      className="group mb-8 md:mb-12 last:mb-0 flex w-full max-w-[52rem]"
+      className="group mb-8 md:mb-12 last:mb-0 w-full max-w-[52rem]"
       ref={ref}
       style={{ scale: scaleProgress, opacity: opacityProgress }}
     >
@@ -36,20 +32,19 @@ export default function Project({
         href={gitghubUrl}
         target="_blank"
         rel="noopener noreferrer"
-        aria-label="View ${title} project on Github"
+        aria-label={`View ${title} project on Github`}
         className="cursor-pointer"
       >
-        <section className="flex flex-col-reverse md:flex-row bg-gray-50 border border-black/5 rounded-lg overflow-hidden md:pr-8 relative md:h-[20rem] hover:bg-gray-100 transition md:group-even:pl-20 text-blue-700 dark:text-blue-400 dark:bg-white/10 dark:hover:bg-white/20 shadow-light-mode dark:shadow-dark-mode">
-          <div className="pt-8 pb-7 px-5 md:pl-14 md:pr-2 md:pt-10 flex flex-col h-full md:max-w-[50%] md:group-even:ml-[20rem]">
+        <section className="flex flex-col md:flex-row bg-gray-50 border border-black/5 rounded-lg overflow-hidden relative hover:bg-gray-100 transition text-blue-700 dark:text-blue-400 dark:bg-white/10 dark:hover:bg-white/20 shadow-light-mode dark:shadow-dark-mode group-even:md:flex-row-reverse gap-4 pt-7 pb-6">
+          <div className="flex-1 pb-4 px-4 md:pb-0 md:pt-0 flex flex-col h-full md:max-w-[35%]">
             <h3 className="text-2xl font-semibold">{title}</h3>
-            {/* leading-relaxed allows for line breaks */}
             <p className="mt-2 mb-2 leading-relaxed text-gray-900 dark:text-gray-200">
               {description}
             </p>
-            <ul className="flex flex-wrap mt-4 gap-2 md:mt-auto items-center justify-center">
+            <ul className="flex flex-wrap mt-4 gap-2">
               {tags.map((tag, index) => (
                 <li
-                  className=" bg-teal-800 dark:bg-teal-900 text-gray-100 dark:text-gray-50 px-3 py-1 text-[0.7rem] uppercase tracking-wider rounded-full"
+                  className="bg-teal-800 dark:bg-teal-900 text-gray-100 dark:text-gray-50 px-3 py-1 text-[0.8rem] uppercase tracking-wider rounded-full"
                   key={index}
                 >
                   {tag}
@@ -57,26 +52,28 @@ export default function Project({
               ))}
             </ul>
           </div>
-          <Image
-            className="w-auto h-[22rem] p-8 md:p-0 object-cover object-top md:h-auto md:w-[34.25rem] md:absolute top-8 -right-44 rounded-t-lg shadow-2xl
-          md:group-even:right-[initial] md:group-even:-left-44
-          md:group-hover:-translate-x-3
-          md:group-hover:translate-y-3
-          md:group-hover:rotate-2
-          group-hover:scale-105
-          md:group-hover:scale-105
-          md:group-even:group-hover:translate x-3
-          md:group-even:group-hover:translate y-3
-          md:group-even:group-hover:rotate-2
-          transition"
-            src={imageUrl}
-            alt={`Screenshot of ${title}. A project that I coded`}
-            width={452}
-            height={300}
-            quality={95}
-            blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk2PHfGAAEYwHsXvURPgAAAABJRU5ErkJggg=="
-            placeholder="blur"
-          />
+          <div className="flex-1 md:flex md:items-center pl-6 pr-6 md:justify-center group-even:md:pr-6 group-even:md:pl-0 group-odd:md:pl-6 group-odd:md:pr-0">
+            <div className="w-full h-[18rem] md:h-[20rem] relative group-even:md:translate-x-6 group-odd:md:-translate-x-6 ">
+              <Image
+                className="object-cover  object-left-top rounded-lg shadow-2xl 
+                  transition 
+                  group-hover:scale-[1.04] 
+                  group-hover:-translate-y-[2%] 
+                  group-hover:rotate-1
+                  group-even:group-hover:translate-x-[2%]
+                  group-even:group-hover:rotate-1
+                  group-odd:group-hover:-translate-x-[2%]
+                  group-odd:group-hover:-rotate-1"
+                src={imageUrl}
+                alt={`Screenshot of ${title}. A project that I coded`}
+                fill
+                sizes="(max-width: 768px) 100vw, 50vw"
+                quality={95}
+                blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk2PHfGAAEYwHsXvURPgAAAABJRU5ErkJggg=="
+                placeholder="blur"
+              />
+            </div>
+          </div>
         </section>
       </a>
     </motion.div>
